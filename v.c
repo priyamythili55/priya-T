063ca012cbcc57ea1064654689f91694baaacc26
priyamythili55 <31530177+priyamythili55@users.noreply.github.com>
2017-09-09 19:26:10 +0530

    Create v.c

---
+++ v.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int ch;
+printf("enter the character");
+scanf("%d",&ch);
+if(ch="a"!!ch="e"!!ch="i"!!ch="o"!!ch="u"!!ch="A"!!ch="E"!!ch="I"!!ch="O"!!ch="U")
+{
+printf("enter the character is vowel");
+}
+else
+{
+printf("enter the character is consonant");
+}
+return 0;
+}
