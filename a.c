aa60f85d01eeffab8a9081caed82a7055e929567
priyamythili55 <31530177+priyamythili55@users.noreply.github.com>
2017-09-11 19:44:46 +0530

    Create a.c

---
+++ a.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("enter the  alphabet");
+scanf("%c",&ch);
+if(ch>=65&&ch<=90)!!(ch>=97&&ch<=122))
+{
+printf("enter the character is alphabet);
+}
+else
+{
+printf("enter the character is not alphabat);
+}
+getch();
+}
