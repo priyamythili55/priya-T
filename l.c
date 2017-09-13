8a55fe51ed5d5dfff3c95a587d84b4157904a895
priyamythili55 <31530177+priyamythili55@users.noreply.github.com>
2017-09-13 19:13:57 +0530

    Create l.c

---
+++ l.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int year;
+printf("enter the year");
+scanf("%d",&year);
+if(year%4==0)
+{
+printf("leap year");
+}
+else
+{
+printf("not leap year");
+}
+getch();
+}
