6770149adaf3aebf96844dc1cbe8bf0528bd016d
priyamythili55 <31530177+priyamythili55@users.noreply.github.com>
2017-09-14 19:31:51 +0530

    Create s.c

---
+++ s.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+include<conio.h>
+void main()
+{
+int i,n,sum=0;
+printf("enter the positive integer");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("sum=%d",sum);
+}
+getch();
+}
