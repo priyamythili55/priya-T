d3999e31ede6c19540af4277cc81d113e889b40e
priyamythili55 <31530177+priyamythili55@users.noreply.github.com>
2017-09-16 19:08:26 +0530

    Create int.c

---
+++ int.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the integer value");
+scanf("%d",&n);
+while(n!=0)
+{
+n=n/10;
+count++
+}
+printf("the number of digit%d",count);
+return 0;
+}
