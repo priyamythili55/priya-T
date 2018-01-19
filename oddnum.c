44cb3c8932d71d14d558a628272da78a27e797ec
priyamythili55 <31530177+priyamythili55@users.noreply.github.com>
2018-01-19 16:06:33 +0530

    Create oddnum.c

---
+++ oddnum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int i,n;
+printf("print odd number till 1 to 5");
+scanf("%d",&n);
+printf("all odd number from 1 to 5");
+for(i=0;i<=1;i++)
+{
+if(i%2!=0)
+{
+printf("%d",i);
+}
+}
