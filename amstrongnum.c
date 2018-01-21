e338a28522109321eeea7030b665b91576021372
priyamythili55 <31530177+priyamythili55@users.noreply.github.com>
2018-01-21 11:15:01 +0530

    Create amstrongnum.c

---
+++ amstrongnum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int n1,n2,rem,i,tep=i,num=0;
+printf("enter the  two intraval");
+scanf("%d %d",&n1,&n2);
+printf("amstrong number between %d are %d":n1,n2);
+for(i=n+1;i<n2;++i)
+{
+while(temp!=0)
+{
+rem=(temp%10);
+num=num+rem*rem*rem;
+temp=temp/10;
+}
+if(i==num)
+{
+printf("%d",i)
+}
+}
+}
+
