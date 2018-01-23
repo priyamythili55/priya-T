c13bd8258ffefd49be120f1526519a8b98cffbef
priyamythili55 <31530177+priyamythili55@users.noreply.github.com>
2018-01-23 16:48:38 +0530

    Create minimum.c

---
+++ minimum.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+int i,smallest,size;
+int arr[5];
+printf(" enter the element");
+scanf("%d",&size);
+printf("enter the element",size);
+for(i=0;i<size;i++)
+{
+scanf("%d",&a[i]);
+}
+smallest=a[0];
+for(i=0;i<size;i++);
+{
+if(smallest>a[i])
+{
+smallest=a[i];
+
+}
+printf("smallest element in  array=%d",smallest);
+}
+}
+}
+
+}
+
