f77f07d3229866e515900f957dc9cb1fe3f5b674
priyamythili55 <31530177+priyamythili55@users.noreply.github.com>
2018-02-01 10:48:34 +0000

    Create minutes.c

---
+++ minutes.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int hours ,minutes,total minutes;
+printf("enter the time in hours and minutes");
+printf("hours");
+scanf("%d",&hours);
+printf("minutes");
+scanf("%d",minutes);
+total minutes=(hours*60)+minutes;
+printf("total time in minutes %d",total minutes);
+}
