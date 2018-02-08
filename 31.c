444974eba02bfbc2feff85c3cb0f89c74e487a2d
priyamythili55 <31530177+priyamythili55@users.noreply.github.com>
2018-02-08 09:23:57 +0000

    Create 31.c

---
+++ 31.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int i,count=0;
+char a[50];
+printf("enter the string");
+scanf("%[^\n]s",a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]!=' ')
+{
+count=count+1;
+}
+printf("%d",count);
+}
