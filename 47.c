13e6fe10bdac312b2843383e1fa7fe7fcb3f5cdc
priyamythili55 <31530177+priyamythili55@users.noreply.github.com>
2018-03-01 16:07:33 +0530

    Create 47.c

---
+++ 47.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+  int i,j,a[100],n,t;
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  scanf("%d",&a[i]);
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<i;j++)
+    {
+      if(a[i]>a[j])
+      {
+        t=a[j];
+        a[j]=a[i];
+        a[i]=t;
+      }
+    }
+  }
+  printf("%d\t%d",a[0],a[n-1]);
+}
