c1ec6e6b83c7f6cff7f1050051859521a7de5ec3
priyamythili55 <31530177+priyamythili55@users.noreply.github.com>
2018-03-01 16:48:00 +0530

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void main()
+{
+  int i;
+  printf("enter the number ");
+  scanf("%d",&i);
+  switch(i)
+  {
+    case 1:
+    printf("one");
+    break;
+    case 2:
+    printf("second");
+     break;
+   case 3:
+    printf("third");
+     break;
+    case 4:
+    printf("four");
+     break;
+    case 5:
+    printf("five");
+     break;
+    case 6:
+    printf("six");
+     break;
+    case 7:
+    printf("seven");
+     break;
+    case 8:
+    printf("eight");
+     break;
+    case 9:
+    printf("nine");
+     break;
+      case 10:
+    printf("ten");
+     break;
+    default :
+    printf("invaild");
+    }
+}
